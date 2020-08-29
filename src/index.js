d190c163c49f56f26c451ab47c11ff4f10d78d74
Rodrigo Pereira <rodrigo.pereira@techifide.com>
2020-08-28 23:09:02 -0400

    more securely

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import express from 'express'
 import routes from './routes'
+import cors from 'cors'
+import helmet from 'helmet'
 import bodyParser from 'body-parser'
-import errorHandler from '../src/shared/middleware/error.middleware'
+import errorHandler from './shared/middleware/error.middleware'
+import notFoundHandler from './shared/middleware/notfound.middleware'
+
 const app = express()
 const PORT = process.env.PORT || 3333
 
 app
+  .use(cors({ origin: process.env.ACCESS_CONTROL_ALLOW_ORIGIN || '*' }))
+  .use(helmet())
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
+  .disable('x-powered-by')
   .use(routes)
   .use(errorHandler)
+  .use(notFoundHandler)
+
   .listen(PORT, () => {
     // eslint-disable-next-line no-console
     console.info(`Server started on port ${PORT} 🚀`)
