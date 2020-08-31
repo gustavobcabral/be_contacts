178dbcb2d247a81aba8488e8f327294e64d990b9
Rodrigo Pereira <rodrigo.pereira@techifide.com>
2020-08-31 12:00:07 -0400

    added log when erros happens

--- src/shared/middleware/error.middleware.js
+++ src/shared/middleware/error.middleware.js
@@ -1,11 +1,24 @@
 import { responseError } from '../helpers/responseGeneric.helper'
 import HttpStatus from 'http-status-codes'
+import simpleLog from 'simple-node-logger'
 
 const errorHandler = (err, req, res, next) => {
   if (!err) return next()
+  const log = simpleLog.createSimpleLogger('errorHandler.log')
   const codStatus = err.httpErrorCode
     ? err.httpErrorCode
     : HttpStatus.INTERNAL_SERVER_ERROR
+  log.error(
+    'error on url: ',
+    req.baseUrl,
+    ' error: ',
+    err,
+    ' at ',
+    new Date().toJSON(),
+    ' request headers: ',
+    req.headers
+  )
+
   res.status(codStatus).json(responseError(err))
 }
 
