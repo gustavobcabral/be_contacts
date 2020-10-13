3a314dfa440a4ec966fc7178e8e5cd46545bda93
Rodrigo Pereira <rodrigo.pereira@techifide.com>
2020-10-13 18:14:31 -0300

    fix bug seq id some tables

--- src/database/seeds/01.responsibility.seed .js
+++ src/database/seeds/01.responsibility.seed .js
@@ -3,6 +3,6 @@ const table = 'responsibility'
 
 exports.seed = async function(knex) {
   await knex(table).del()
-  await knex.raw(`ALTER SEQUENCE ${table}_id_seq RESTART WITH 1`)
+  await knex.raw(`ALTER SEQUENCE ${table}_id_seq RESTART WITH 5`)
   return knex(table).insert(data)
 }
--- src/database/seeds/05.languages.seed.js
+++ src/database/seeds/05.languages.seed.js
@@ -3,6 +3,6 @@ const table = 'languages'
 
 exports.seed = async function(knex) {
   await knex(table).del()
-  await knex.raw(`ALTER SEQUENCE ${table}_id_seq RESTART WITH 1`)
+  await knex.raw(`ALTER SEQUENCE ${table}_id_seq RESTART WITH 4`)
   return knex(table).insert(data)
 }
