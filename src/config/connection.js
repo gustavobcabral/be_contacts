293e60f5d534f0631d73e332c2cd522d7c8b21aa
Rodrigo Pereira <pereirapy@hotmail.com>
2022-03-11 00:24:16 +0700

    Fix bug with date

--- src/config/connection.js
+++ src/config/connection.js
@@ -2,6 +2,9 @@
 const setupKnexPaginator = require('./knexPaginator')
 const env = process.env.NODE_ENV || 'development'
 const knexfile = require('./knexfile')
+var types = require('pg').types;
+// override parsing date column to Date()
+types.setTypeParser(1082, val => val); 
 const knex = require('knex')(knexfile[env])
 setupKnexPaginator(knex)
 module.exports = knex
